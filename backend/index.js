9b0eac93a1fd677789c5c5b7a10acb92e6d48e47
Farhan Shaikh <programmingwithfarhan@gmail.com>
2025-12-05 23:12:57 +0530

    remove trailing /

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const io = new Server(httpServer, {
         : [
             "http://localhost:5173",
             "http://127.0.0.1:5173",
-            "https://fourinrow.vercel.app/",
+            "https://fourinrow.vercel.app",
             "https://fourinrow.farhanshaikh.life",
           ],
   },
